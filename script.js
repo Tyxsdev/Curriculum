0072b51fb7d400d8b59cf4a04d8fe7c79a4efeaf
Ulises Sosa <ulisesdevsosa@gmail.com>
2023-01-20 19:03:27 -0300

    Crea dos divs mas y los mustra de la misma forma

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const spans = document.querySelectorAll('.clickable');
-const divs = document.querySelectorAll('.hidden');
+const primerSpan = document.querySelectorAll('span');
+const primerDiv = document.querySelector('.primer-div');
+const segundoDiv = document.querySelector('.segundo-div');
+const tercerDiv = document.querySelector('.tercer-div');
+const divs = document.querySelectorAll('div');
 const closeSymbol = document.querySelectorAll('.material-symbols-outlined');
 
 function mostrarMas(e){
-   divs.forEach(div => div.style.display='block');
+    let contenido = e.target.textContent;
+    if (contenido === 'Hospital San Antonio'){
+        primerDiv.style.display = 'block';
+    } else if (contenido === 'PEP Solutions'){
+        segundoDiv.style.display = 'block';
+    } else if (contenido === 'Floyd bar'){
+        tercerDiv.style.display = 'block';
+    }
 }
-spans.forEach(span => span.addEventListener('click', mostrarMas));
+
+primerSpan.forEach(span => span.addEventListener('click', mostrarMas))
 
 function cerrar(e){
     divs.forEach(div => div.style.display='none');
